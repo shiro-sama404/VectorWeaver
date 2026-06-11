eac707976362dae40e03393040f68bd4e3ddd58b
Arthur Barbosa <arthurbarbosaandrade2003@gmail.com>
2026-06-11 02:46:47 -0400

    (feature) json manipulation

---
+++ include/vectorweaver/json_converter.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <unordered_map>
+#include "json.hpp"
+
+namespace vw
+{
+/// @brief Utilitário estático para serialização e desserialização de relógios.
+class JsonConverter
+{
+public:
+    /// @brief Converte o mapa do Relógio Vetorial para um objeto JSON.
+    /// @param clock_map
+    static nlohmann::json map_to_json(const ClockState& clock_map)
+    {
+        auto j = nlohmann::json::object();
+        for (const auto& [key, val] : clock_map)
+            j[std::to_string(key)] = val;
+        return j;
+    }
+
+    /// @brief Reconstrói o mapa numérico a partir do JSON.
+    /// @param json_clock
+    static ClockState json_to_map(const nlohmann::json& json_clock)
+    {
+        ClockState clock_map;
+        for (const auto& [key, val] : json_clock.items())
+            clock_map[std::stoi(key)] = val.get<int>();
+        return clock_map;
+    }
+};
+} // namespace vw
